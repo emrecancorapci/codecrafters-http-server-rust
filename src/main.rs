55961616f516bccd5f9658f1d1dc089268600e42
Emre Can Çorapçı <emrecancorapci@gmail.com>
2024-05-27 00:29:30 +0300

    Add handle_connection function

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,39 @@
-// Uncomment this block to pass the first stage
-use std::net::TcpListener;
+use std::{
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+};
 
 fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
-    
-    let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
-    
-    for stream in listener.incoming() {
-        match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
-            }
-            Err(e) => {
-                println!("error: {}", e);
-            }
-        }
+  let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
+
+  for stream in listener.incoming() {
+    println!("connection accepted. Stream: {:?}", stream);
+
+    match stream {
+      Ok(_stream) => {
+        println!("accepted new connection");
+        handle_connection(_stream);
+      }
+      Err(e) => {
+        println!("error: {}", e);
+      }
     }
+  }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+    println!("HTTP request: {:?}", http_request);
+
+    let http_version = "HTTP/1.1";
+    let status_code = "200";
+    let status_message = "OK";
+    let response = format!("{http_version} {status_code} {status_message}\r\n\r\n");
+
+    stream.write_all(response.as_bytes()).unwrap();
 }

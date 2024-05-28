654ba00255ea2d08b1bab4c316340efffb07a67d
Emre Can Çorapçı <emrecancorapci@gmail.com>
2024-05-28 17:24:18 +0300

    Refactor to separate file route

--- src/routes.rs
+++ src/routes.rs
@@ -1,13 +1,16 @@
-use crate::{ controller, http::{ response, Request } };
+use crate::http::{ response, Request };
 
 mod echo;
+mod file;
 
 pub fn router(http_request: &Request) -> String {
-    match http_request.request.path_array[0] {
+    let path = &http_request.request.path_array;
+
+    match path[0] {
         "" => { response::ok("", "") }
         "user-agent" => { response::ok(http_request.user_agent, "text/plain") }
         "echo" => { echo::router(http_request) }
-        "files" => { controller::file::get(http_request) }
+        "files" => { file::router(http_request) }
         _ => { response::not_found() }
     }
 }
---
+++ src/routes/file.rs
@@ -0,0 +1,8 @@
+use crate::{ controller::file::get, http::{ response, Request } };
+
+pub fn router(http_request: &Request) -> String {
+    match http_request.request.method {
+        "GET" => { get(http_request) }
+        _ => { response::method_not_allowed() }
+    }
+}

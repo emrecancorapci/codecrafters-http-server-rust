b820c79394c83b26a83f28f39a7a9fc4c7b0d9c3
Emre Can Çorapçı <emrecancorapci@gmail.com>
2024-05-28 14:03:59 +0300

    Add HttpRequest struct, Refactor whole codebase to use the struct,

--- src/endpoint.rs
+++ src/endpoint.rs
@@ -1,40 +1,22 @@
-use crate::request::{ bad_request, send_content };
+use crate::{request::{ bad_request, not_found, send_content }, HttpRequest};
 use std::{ env, fs };
 
-pub fn user_agent(http_request: Vec<String>) -> String {
-    let user_agent_str = http_request.get(2);
+pub fn echo(http_request: &HttpRequest) -> String {
+    let echo_text = http_request.path_array.get(1);
 
-    if user_agent_str.is_none() {
-        return bad_request("user_agent_str not found");
+    match echo_text {
+        None => return bad_request("echo_text not found"),
+        Some(echo_text) => {
+            if echo_text.is_empty() {
+                return bad_request("echo_text is empty");
+            }
+            return send_content(echo_text, "text/plain");
+        }
     }
-
-    let user_agent = user_agent_str.unwrap().split(' ').nth(1);
-
-    if user_agent.is_none() {
-        return bad_request("user_agent not found");
-    }
-
-    return send_content(user_agent.unwrap(), "text/plain");
 }
 
-pub fn echo(http_request: Vec<String>) -> String {
-    let echo_text_str = http_request.get(0);
-
-    if echo_text_str.is_none() {
-        return bad_request("echo_text_str not found");
-    }
-
-    let echo_text: Option<&str> = echo_text_str.unwrap().split(' ').nth(2);
-
-    if echo_text.is_none() {
-        return bad_request("echo_text not found");
-    }
-
-    return send_content(echo_text.unwrap(), "text/plain");
-}
-
-pub fn files(http_request: Vec<String>) -> String {
-    let file_name_str = http_request.get(0);
+pub fn files(http_request: &HttpRequest) -> String {
+    let file_name = http_request.path_array[1];
     let env_args: Vec<String> = env::args().collect();
 
     let mut directory = {
@@ -47,20 +29,6 @@ pub fn files(http_request: Vec<String>) -> String {
 
     println!("directory: {directory}");
 
-    if file_name_str.is_none() {
-        return bad_request("file_name_str not found");
-    }
-
-    let file_name = {
-        let file_name = file_name_str.unwrap().split(' ').nth(1);
-
-        if file_name.is_none() {
-            return bad_request("--directory is not set");
-        } else {
-            file_name.unwrap()
-        }
-    };
-
     println!("file_name: {file_name}");
 
     directory.push_str(&file_name);
@@ -68,7 +36,7 @@ pub fn files(http_request: Vec<String>) -> String {
     let file = fs::read_to_string(directory);
 
     if file.is_err() {
-        return bad_request("file not found");
+        return not_found();
     }
 
     return send_content(&file.unwrap(), "application/octet-stream");
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,18 @@
 use std::{ io::{ prelude::*, BufReader }, net::{ TcpListener, TcpStream }, thread };
 
+use itertools::Itertools;
+
 pub mod endpoint;
 pub mod request;
+
+pub struct HttpRequest<'a>{
+    method: &'a str,
+    path: &'a str,
+    http_version: &'a str,
+    host: &'a str,
+    user_agent: &'a str,
+    path_array: Vec<&'a str>,
+}
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
@@ -23,36 +34,24 @@ fn main() {
 
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
-    let http_request: Vec<_> = buf_reader
+    let raw_http_request: Vec<_> = buf_reader
         .lines()
         .map(|result| result.unwrap())
         .take_while(|line| !line.is_empty())
         .collect();
-    println!("HTTP request: {:?}", http_request);
+    println!("HTTP request: {:?}", raw_http_request);
 
-    let response = generate_reponse(&http_request);
+    let http_request = &format_request(&raw_http_request);
+
+    let response = router(http_request);
+
     println!("Response: {response}");
+
     stream.write_all(response.as_bytes()).unwrap();
 }
 
-fn generate_reponse(http_request: &Vec<String>) -> String {
-    let request_line = {
-        let request_line = http_request.get(0);
-
-        if request_line.is_none() {
-            return request::bad_request("request_line not found");
-        } else {
-            request_line.unwrap().split(' ').collect::<Vec<&str>>()
-        }
-    };
-
-    let path = request_line[1][1..].split('/').collect::<Vec<&str>>();
-
-    router(http_request.clone(), path[0])
-}
-
-fn router(http_request: Vec<String>, path: &str, ) -> String {
-    match path {
+fn router(http_request: &HttpRequest) -> String {
+    match http_request.path_array[0] {
         "" => {
             return request::ok("");
         }
@@ -60,7 +59,7 @@ fn router(http_request: Vec<String>, path: &str, ) -> String {
             return endpoint::echo(http_request);
         }
         "user-agent" => {
-            return endpoint::user_agent(http_request);
+            return request::send_content(http_request.user_agent, "text/plain");
         }
         "files" => {
             return endpoint::files(http_request);
@@ -68,5 +67,26 @@ fn router(http_request: Vec<String>, path: &str, ) -> String {
         _ => {
             return request::not_found();
         }
+    }
+}
+
+fn format_request(http_request: &Vec<String>) -> HttpRequest {
+    let request_line = {
+        let request_line = http_request.get(0);
+
+        if request_line.is_none() {
+            panic!("request_line not found")
+        } else {
+            request_line.unwrap().split(' ').collect_vec()
+        }
+    };
+
+    HttpRequest {
+        method: request_line[0],
+        http_version: request_line[2],
+        user_agent: http_request.get(2).unwrap().split(' ').nth(1).unwrap(),
+        host: http_request.get(1).unwrap().split(' ').nth(1).unwrap(),
+        path: request_line[1],
+        path_array: request_line[1][1..].split('/').collect_vec(),
     }
 }

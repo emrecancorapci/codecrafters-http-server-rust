0d26228380dfce78aa4c4c31e30d37dde91aac9b
Emre Can Çorapçı <emrecancorapci@gmail.com>
2024-05-30 20:28:28 +0300

    Add encoding support to echo endpoint

--- src/controller/echo.rs
+++ src/controller/echo.rs
@@ -1,6 +1,11 @@
 use std::collections::HashMap;
 
-use crate::http::{ response::{ HttpResponse, StatusCode }, request::HttpRequest };
+use itertools::Itertools;
+
+use crate::{
+    extensions::hash_map::HashMapExt,
+    http::{ request::HttpRequest, response::{ HttpResponse, StatusCode } },
+};
 
 pub fn get(http_request: &HttpRequest) -> String {
     let echo_text = http_request.request.path_array[1];
@@ -12,24 +17,35 @@ pub fn get(http_request: &HttpRequest) -> String {
             .to_string();
     }
 
-    let headers: HashMap<String, String> = {
-        let mut headers = HashMap::new();
-        let accept_encoding = http_request.headers.get("accept-encoding");
+    match get_encoding(http_request) {
+        Some(encoders) => {
+            println!("Encodings : {}", encoders.to_string());
+            return HttpResponse::from(&StatusCode::Ok)
+                .text(echo_text)
+                .headers(encoders)
+                .to_string();
+        }
+        None => {
+            return HttpResponse::from(&StatusCode::Ok).text(echo_text).to_string();
+        }
+    }
+}
 
-        match accept_encoding {
-            None => headers,
-            Some(encoding) => {
-                if encoding != "invalid-encoding" {
-                    headers.insert(String::from("Content-Encoding"), encoding.to_string());
-                }
-                headers
+fn get_encoding(http_request: &HttpRequest) -> Option<HashMap<String, String>> {
+    let mut headers: HashMap<String, String> = HashMap::new();
+    let accept_encoding = http_request.headers.get("accept-encoding");
+
+    if accept_encoding.is_some() {
+        let encodings = accept_encoding.unwrap().split(',').collect_vec();
+
+        for encoding in encodings {
+            if encoding.trim() == "gzip" {
+                headers.insert(String::from("Content-Encoding"), String::from("gzip"));
+                break;
             }
         }
-    };
-
-    if headers.len() > 0 {
-        return HttpResponse::from(&StatusCode::Ok).headers(headers).text(echo_text).to_string();
-    } else {
-        return HttpResponse::from(&StatusCode::Ok).text(echo_text).to_string();
+        return Some(headers);
     }
+
+    None
 }

da1948c3a0cec333e31fcb682147ecc3d14076f6
Emre Can Çorapçı <emrecancorapci@gmail.com>
2024-05-30 21:37:32 +0300

    Add gzip compressing

--- src/controller/echo.rs
+++ src/controller/echo.rs
@@ -1,11 +1,9 @@
 use std::collections::HashMap;
-
 use itertools::Itertools;
+use std::io::prelude::*;
+use flate2::{ write::GzEncoder, Compression };
 
-use crate::{
-    extensions::hash_map::HashMapExt,
-    http::{ request::HttpRequest, response::{ HttpResponse, StatusCode } },
-};
+use crate::http::{ request::HttpRequest, response::{ HttpResponse, StatusCode } };
 
 pub fn get(http_request: &HttpRequest) -> String {
     let echo_text = http_request.request.path_array[1];
@@ -18,12 +16,22 @@ pub fn get(http_request: &HttpRequest) -> String {
     }
 
     match get_encoding(http_request) {
-        Some(encoders) => {
-            println!("Encodings : {}", encoders.to_string());
-            return HttpResponse::from(&StatusCode::Ok)
-                .text(echo_text)
-                .headers(encoders)
-                .to_string();
+        Some(encoding_header) => {
+            match encode(echo_text) {
+                Ok(compressed_string) => {
+                    return HttpResponse::from(&StatusCode::Ok)
+                        .text(&compressed_string)
+                        .headers(encoding_header)
+                        .to_string();
+                }
+
+                Err(e) => {
+                    return HttpResponse::from(&StatusCode::InternalServerError)
+                        .text(&e)
+                        .debug()
+                        .to_string();
+                }
+            }
         }
         None => {
             return HttpResponse::from(&StatusCode::Ok).text(echo_text).to_string();
@@ -48,4 +56,31 @@ fn get_encoding(http_request: &HttpRequest) -> Option<HashMap<String, String>> {
     }
 
     None
+}
+
+fn encode(echo_text: &str) -> Result<String, String> {
+    let mut encoder = GzEncoder::new(Vec::new(), Compression::default());
+
+    match encoder.write_all(echo_text.as_bytes()) {
+        Ok(_) => {}
+        Err(e) => {
+            return Err(format!("Error: {}", e));
+        }
+    }
+
+    match encoder.finish() {
+        Ok(compressed_bytes) => {
+            match String::from_utf8(compressed_bytes) {
+                Ok(compressed_string) => {
+                    return Ok(compressed_string);
+                }
+                Err(e) => {
+                    return Err(format!("Error: {}", e));
+                }
+            }
+        }
+        Err(e) => {
+            return Err(format!("Error: {}", e));
+        }
+    }
 }
